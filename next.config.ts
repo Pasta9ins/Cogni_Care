1c33daefaf2b7b6d872321d78fd47b6613e17919
Pasta9ins <anirudhachaudhary33@gmail.com>
2025-06-13 22:32:15 +0530

    reverted back rewrites

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
-
-//removed at 10.15
-  //   async rewrites() {
-//         return [
-//             {
-//                 source: '/api/:path*', // Match any request starting with /api/
-//                 destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`, // Proxy it to your backend
-//             },
-//         ];
-//     },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*', // Match any request starting with /api/
+                destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`, // Proxy it to your backend
+            },
+        ];
+    },
 };
 
 export default nextConfig;
